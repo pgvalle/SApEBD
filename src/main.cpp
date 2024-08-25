af48e384639e33215f654991504302cad26a4739
pgvalle <pgvalle.nogueira@gmail.com>
2024-08-25 14:53:11 -0300

    new version

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,16 @@
 #include <TimeLib.h>  // Time by Michael Margolis
 #include <TimeAlarms.h>  // TimeAlarms by Michael Margolis
 
-#define DELAY_INTERVAL 10  // milliseconds
+#define DELAY_INTERVAL 500  // milliseconds
+#define SYNC_INTERVAL 1440
 
 #define RING_DURATION 5  // seconds
 #define RELAY 8
 #define LED   13
 
 // Your WiFi credentials
-#define SSID "IBS"
-#define PASS "cristovida1234"
+#define SSID "ssid"
+#define PASS "pass"
 
 enum TimeStatus {
   TIME_OK, TIME_IMPRECISE, TIME_WRONG
@@ -22,16 +23,15 @@ enum TimeStatus {
 // STATE VARIABLES //
 
 SoftwareSerial espSerial(10, 11);  // RX, TX
-bool alarmRinging = false;
 TimeStatus t1meStatus = TIME_WRONG;
-bool ledOn = false;
-int ledTicks = 0;
+int precisionLoss = 0;
+time_t unixTime = 0;
 
 // FUNCTIONS //
 
-void alarmRing();
-void timeShowStatus();
-time_t timeSync();
+void ringAlarm();
+void showTimeStatus();
+time_t syncTime();
 
 // ENTRY POINT //
 
@@ -41,83 +41,98 @@ void setup() {
   // setup pins
   pinMode(LED, OUTPUT);
   pinMode(RELAY, OUTPUT);
-  digitalWrite(RELAY, alarmRinging);
+  digitalWrite(RELAY, false);
 
   // Initialize the WiFiEsp library with the software serial
   espSerial.begin(9600);
   WiFi.init(&espSerial);
 
   // configure TimeLib with rtc
-  setSyncInterval(2880);  // sync once each 2 days
-  setSyncProvider(timeSync);
+  setTime(0);
+  setSyncInterval(SYNC_INTERVAL);  // sync once each 2 days
+  setSyncProvider(syncTime);
 
-  Alarm.alarmRepeat(dowSunday, 9, 0, 0, alarmRing);  // EBD início
-  Alarm.alarmRepeat(dowSunday, 11, 0, 0, alarmRing);  // EBD final
-  Alarm.alarmRepeat(dowSunday, 11, 10, 0, alarmRing);  // Domingo manhã
-  Alarm.alarmRepeat(dowSunday, 18, 30, 0, alarmRing);  // Domingo noite
-  Alarm.alarmRepeat(dowWednesday, 19, 30, 0, alarmRing);  // Quarta
+  Alarm.alarmRepeat(dowSunday, 9, 0, 0, ringAlarm);  // EBD início
+  Alarm.alarmRepeat(dowSunday, 11, 0, 0, ringAlarm);  // EBD final
+  Alarm.alarmRepeat(dowSunday, 11, 10, 0, ringAlarm);  // EBD final 2
+  Alarm.alarmRepeat(dowSunday, 18, 30, 0, ringAlarm);  // Domingo noite
+  Alarm.alarmRepeat(dowWednesday, 19, 30, 0, ringAlarm);  // Quarta
 
-  // Alarm.alarmRepeat(dowSunday, 8, 23, 30, alarmRing);  // test
+  //Alarm.alarmRepeat(dowSunday, 13, 54, 30, ringAlarm);  // test
 }
 
 void loop() {
-  timeShowStatus();
+  showTimeStatus();
   Alarm.delay(DELAY_INTERVAL);
 }
 
 // FUNCTIONS DEFINITIONS //
 
-void alarmRing() {
+void ringAlarm() {
+  if (t1meStatus == TIME_WRONG)
+    return;
+
   digitalWrite(RELAY, true);
-  alarmRinging = true;
 
   Alarm.timerOnce(RING_DURATION, []() {
     digitalWrite(RELAY, false);
-    alarmRinging = false;
   });
 }
 
-void timeShowStatus() {
+void showTimeStatus() {
   int ticks2wait;
   switch (t1meStatus) {
     case TIME_OK:
       digitalWrite(LED, HIGH);
       return;
     case TIME_IMPRECISE:
-      ticks2wait = 500 / DELAY_INTERVAL;
+      ticks2wait = 2;
       break;
     case TIME_WRONG:
-      ticks2wait = 250 / DELAY_INTERVAL;
+      ticks2wait = 1;
       break;
   default:
     break;
   }
 
-  if (++ledTicks % ticks2wait == 0) {
+  // blink
+  static int ledTicks = 0;
+
+  if (++ledTicks == ticks2wait) {
+    static bool ledOn = false;
+
     ledOn = !ledOn;
     ledTicks = 0;
     digitalWrite(LED, ledOn);
   }
 }
 
-time_t timeSync() {
-  static int precisionLoss = 0;
+void failSafe() {
+  if (t1meStatus == TIME_OK)
+    t1meStatus = TIME_IMPRECISE;
+  else if (t1meStatus == TIME_IMPRECISE) {
+    if (++precisionLoss >= 5)
+      t1meStatus = TIME_WRONG;
+  }
 
-  if (WiFi.status() != WL_CONNECTED) WiFi.begin(SSID, PASS);  // try once
+  unixTime += SYNC_INTERVAL + 5;  // aproximation. Precission loss
+}
+
+time_t syncTime() {
+  if (WiFi.status() != WL_CONNECTED)
+    WiFi.begin(SSID, PASS);  // try once
+
   if (WiFi.status() != WL_CONNECTED) {
-    if (t1meStatus == TIME_OK) t1meStatus = TIME_IMPRECISE;
-    else if (precisionLoss >=  7) t1meStatus = TIME_WRONG;
-    else if (t1meStatus == TIME_IMPRECISE) precisionLoss++;
-    return now();
+    failSafe();
+    return unixTime;
   }
 
   WiFiEspClient client;
+  client.setTimeout(5000);
   client.connect("worldtimeapi.org", 80);
   if (!client.connected()) {
-    if (t1meStatus == TIME_OK) t1meStatus = TIME_IMPRECISE;
-    else if (precisionLoss >=  7) t1meStatus = TIME_WRONG;
-    else if (t1meStatus == TIME_IMPRECISE) precisionLoss++;
-    return now();
+    failSafe();
+    return unixTime;
   }
 
   client.println("GET /api/timezone/America/Sao_Paulo HTTP/1.1");
@@ -129,12 +144,12 @@ time_t timeSync() {
   client.find((char *)"unixtime\":");
 
   String unixTimeStr = client.readStringUntil(',');
-  const time_t unixTime = unixTimeStr.toInt() - 10800;
+
+  precisionLoss = 0;
+  t1meStatus = TIME_OK;
+  unixTime = unixTimeStr.toInt() - 10800;
 
   client.stop();
-
-  t1meStatus = TIME_OK;
-  precisionLoss = 0;
 
   return unixTime;
 }
