c79c755d1fabcc0e51c8d91a17a0e1df0d8f5b84
pgvalle <pgvalle.nogueira@gmail.com>
2024-09-04 15:11:05 -0300

    page responses common stuff in separated string and alarm working

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,14 @@
 #include <WiFiUdp.h>
 
 #define HOSTNAME "sinal"
-#define RELAY 2
-#define LED   3
+#define RELAY 5
+#define LED   4
 #define UPDATE_INTERVAL 250
 #define RECONNECTION_TIMEOUT 300000  // 5 min
 
+// alarm stuff
+bool isAlarming = false;
+uint64_t alarmStart = 0;
 // in case it's not connected to a network
 bool triedReconnection = false;
 uint64_t timeLastReconnection = 0;
@@ -25,20 +28,40 @@ WiFiUDP ntpUDP;
 NTPClient ntpClient(ntpUDP);
 // web server
 ESP8266WebServer webServer(80);
+// page base head tags
+String pageCommon = R"(
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <style>
+      body { font-family: Arial, sans-serif; text-align: center; }
+      button { padding: 10px 20px; margin: 10px; }
+      input { padding: 10px; margin: 10px; }
+    </style>
+    <meta charset='UTF-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Sinal</title>
+)";
 
-bool saveWiFiConf();
-void loadWiFiConf();
+// alarm stuff
+bool checkDOWTime(int dow, int h, int m, int s);
+void try2Alarm();
+// wifi stuff
 void tryConnecting2WiFi();
 // Pages handlers
 void handleNotFound();
 void handleRoot();
-void handleAlarms();
+void handleRing();
 void handleSave();
 void handleReboot();
 
 
 void setup() {
   Serial.begin(9600);
+
+  pinMode(RELAY, OUTPUT);
+  pinMode(LED, OUTPUT);
 
   const size_t memSize = sizeof(ssid) + sizeof(pass) - 2;
   //EEPROM.begin(memSize);
@@ -47,10 +70,13 @@ void setup() {
   WiFi.softAP(HOSTNAME, "sinalconf", 1, false, 1);
 
   // load wifi config
-  // for (int i = 0; i < 16; i++) {
-  //   ssid[i] = EEPROM.read(i);
-  //   pass[i] = EEPROM.read(i + 16);
-  // }
+  /*for (int i = 0; i < 16; i++) {
+    ssid[i] = EEPROM.read(i);
+    pass[i] = EEPROM.read(i + 16);
+  }*/
+
+  Serial.println("  ssid: " + String(ssid));
+  Serial.println("  pass: " + String(pass));
 
   tryConnecting2WiFi();
 
@@ -58,6 +84,7 @@ void setup() {
   webServer.begin();
   webServer.onNotFound(handleNotFound);
   webServer.on("/", HTTP_GET, handleRoot);
+  webServer.on("/ring", HTTP_GET, handleRing);
   webServer.on("/save", HTTP_GET, handleSave);
   webServer.on("/reboot", HTTP_GET, handleReboot);
 
@@ -74,6 +101,8 @@ void loop() {
   webServer.handleClient();
   ntpClient.update();  // time sync
   MDNS.update();  // hostname
+
+  try2Alarm();
 
   // try reconnecting if not connected
   if (!WiFi.isConnected() && !triedReconnection) {
@@ -92,16 +121,75 @@ void loop() {
     if (delta > RECONNECTION_TIMEOUT)
       triedReconnection = false;
   }
+
+  Serial.println(ntpClient.getFormattedTime());
+  delay(500);
 }
 
 
-bool saveWiFiConf() {
-  for (int i = 0; i < 12; i++) {
-    EEPROM.put(0, ssid[i]);
-    EEPROM.put(12, pass[i]);
+bool checkDOWTime(int dow, int h, int m, int s) {
+  return ntpClient.getDay() == dow &&
+         ntpClient.getHours() == h &&
+         ntpClient.getMinutes() == m &&
+         ntpClient.getSeconds() == s;
+}
+
+void try2Alarm() {
+  if (isAlarming) {
+    if (millis() - alarmStart >= 5000) {  // 5 seconds alarming
+      digitalWrite(RELAY, LOW);
+      isAlarming = false;
+    }
+    return;
   }
 
-  return EEPROM.commit();
+  // 9:00:00  // inicie ebd
+  // 10:55:00  // ebd final 1
+  // 11:05:00  // ebd final 2
+  // 11:15:00  // ebd final 3
+  // 18:30:00 // domingo noite
+  // 19:30:00 // quarta
+  if (checkDOWTime(0, 9, 0, 0)) {
+    digitalWrite(RELAY, HIGH);
+    isAlarming = true;
+    alarmStart = millis();
+    return;
+  }
+
+  if (checkDOWTime(0, 10, 55, 0)) {
+    digitalWrite(RELAY, HIGH);
+    isAlarming = true;
+    alarmStart = millis();
+    return;
+  }
+
+  if (checkDOWTime(0, 11, 05, 0)) {
+    digitalWrite(RELAY, HIGH);
+    isAlarming = true;
+    alarmStart = millis();
+    return;
+  }
+
+  if (checkDOWTime(0, 11, 15, 0)) {
+    digitalWrite(RELAY, HIGH);
+    isAlarming = true;
+    alarmStart = millis();
+    return;
+  }
+
+  if (checkDOWTime(0, 18, 30, 0)) {
+    digitalWrite(RELAY, HIGH);
+    isAlarming = true;
+    alarmStart = millis();
+    return;
+  }
+
+  if (checkDOWTime(3, 19, 30, 0)) {
+    digitalWrite(RELAY, HIGH);
+    isAlarming = true;
+    alarmStart = millis();
+    return;
+  }
 }
 
 void tryConnecting2WiFi() {
@@ -114,51 +202,57 @@ void tryConnecting2WiFi() {
 }
 
 void handleNotFound() {
-  webServer.send(404, "text/html", "Página não encontrada");
+  webServer.send(404, "text/html", pageCommon + R"(
+    </head>
+    <body>
+      <h1>Página não encontrada</h1>
+      <form action='/' method='GET'>
+        <button type='submit'>página inicial</button>
+      </form>
+    </body>
+    </html>
+  )");
 }
 
 void handleRoot() {
-  String root = R"(
-    <html>
-    <head>
-      <style>
-        body { font-family: Arial, sans-serif; text-align: center; }
-        button { padding: 10px 20px; margin: 10px; }
-        input { padding: 10px; margin: 10px; }
-      </style>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Sinal</title>
+  Serial.println("/");
+  webServer.send(200, "text/html", pageCommon + R"(
     </head>
     <body>
       <h1>Sinal Autônomo</h1>
       <form action='/save' method='GET'>
-        <input type='text' name='ssid' placeholder='Nova rede ()";
-  
-  root += ssid;
-
-  root += R"()'><br>
+        <input type='text' name='ssid' placeholder='Nova rede ()" + ssid + R"()'><br>
         <input type='password' name='pass' placeholder='Nova senha'><br>
         <button type='submit'>salvar</button>
       </form>
-      <form action='/alarms' method='GET'>
-        <button type='submit'>alarmes</button>
+      <form action='/ring' method='GET'>
+        <button type='submit'>tocar (5s)</button>
       </form>
       <form action='/reboot' method='GET'>
         <button type='submit'>reiniciar</button>
       </form>
     </body>
     </html>
-  )";
-
-  Serial.println("/");
-  webServer.send(200, "text/html", root);
+  )");
 }
 
-void handleAlarms() {
-  Serial.println("/alarms");
-  webServer.send(200, "text/html", "In progress...");
+void handleRing() {
+  // only alarm now if not alarming
+  if (!isAlarming) {
+    digitalWrite(RELAY, HIGH);
+    isAlarming = true;
+    alarmStart = millis();
+  }
+
+  Serial.println("/ring");
+  webServer.send(200, "text/html", pageCommon + R"(
+      <meta http-equiv='refresh' content='5; url=/'>
+    </head>
+    <body>
+      <h1>Tocando</h1>
+    </body>
+    </html>
+  )");
 }
 
 void handleSave() {
@@ -166,60 +260,37 @@ void handleSave() {
   String password = webServer.arg("pass");  // Get value from the second text input
 
   Serial.println("/save");
-  // just for debug now
-  Serial.println("  ssid: " + network);
-  Serial.println("  pass: " + password);
+  Serial.println("  new ssid: " + network);
+  Serial.println("  new pass: " + password);
 
-  /*
-  for (int i = 0; i < 12; i++) {
+  /*for (int i = 0; i < 12; i++) {
     EEPROM.put(0, ssid[i]);
     EEPROM.put(12, pass[i]);
   }*/
 
   if (EEPROM.commit())
-    webServer.send(200, "text/html", R"(
-      <!DOCTYPE html>
-      <html lang="br">
-      <head>
-        <style>
-          body { font-family: Arial, sans-serif; text-align: center; }
-          button { padding: 10px 20px; margin: 10px; }
-          input { padding: 10px; margin: 10px; }
-        </style>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    webServer.send(200, "text/html", pageCommon + R"(
         <meta http-equiv='refresh' content='10; url=/'>
-        <title>Sinal</title>
       </head>
       <body>
-        <h1>Nova rede salva</h1>
-        Reiniciando em 10 segundos
+        <h1>Configurações salvas</h1>
+        <p>Reiniciando em 10 segundos</p>
       </body>
       </html>
     )");
   else
-    webServer.send(200, "text/html", R"(Erro ao salvar)");
-
-  // if data saved, restart automatically
-  // else tell something went wrong
+    webServer.send(200, "text/html", pageCommon + R"(
+      </head>
+      <body>
+        <h1>Falha ao salvar configurações</h1>
+      </body>
+      </html>
+    )");
 }
 
 void handleReboot() {
-  webServer.send(200, "text/html", R"(
-    <!DOCTYPE html>
-    <html lang="br">
-    <head>
-      <style>
-        body { font-family: Arial, sans-serif; text-align: center; }
-        button { padding: 10px 20px; margin: 10px; }
-        input { padding: 10px; margin: 10px; }
-      </style>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  webServer.send(200, "text/html", pageCommon + R"(
       <meta http-equiv='refresh' content='10; url=/'>
-      <title>Sinal</title>
     </head>
     <body>
       <h1>Reiniciando...</h1>
